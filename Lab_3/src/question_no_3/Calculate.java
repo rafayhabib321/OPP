ba658e0b73c402c8bed51e1e96855a4df30c6f4d
rafayhabib321 <rafymalik321@gmail.com>
2025-03-12 12:39:15 +0500

    3/12/2025

---
+++ Lab_3/src/question_no_3/Calculate.java
@@ -0,0 +1,13 @@
+package question_no_3;
+
+public class Calculate {
+	
+public int add (int a,int b) {
+	int res = a+b;
+	return res;
+}
+public int add (int a,int b,int c) {
+	int res = a+b+c;
+	return res;
+}
+}
---
+++ Lab_3/src/question_no_3/Main.java
@@ -0,0 +1,15 @@
+package question_no_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calculate c = new Calculate();
+		
+		System.out.println("Method Overloading");
+		System.out.println("->"+c.add(16, 58));
+		System.out.println("->"+c.add(55, 55, 55));
+
+	}
+
+}
